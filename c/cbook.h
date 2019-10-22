e777e128eefe100384a6774ce824456f8fbe9677
Senthilkumaran <skdudeflow@gmail.com>
2019-10-22 19:17:38 +0530

    c notepad

--- c/cbook.h
+++ c/cbook.h
@@ -7,30 +7,18 @@ char createBook(){
 
 char bname[100];
 char text[100];
-int id,max,i;
+int id,max,i,j;
 char buf[50];
+char ch;
 
 printf("\nPlease enter book name:");
-fgets(bname,100,stdin);;
-
-//to uppercase
-for(i=0; bname[i]!='\0'; i++)
-    {
-        /*
-         * If current character is lowercase alphabet then
-         * convert it to uppercase.
-         */
-        if(bname[i]>='a' && bname[i]<='z')
-        {
-            bname[i] = bname[i] - 32;
-        }
-    }
+scanf ("%s",bname);
 
 FILE *createfile,*checkfile;
 
 //check file max id
 
-if ((checkfile = fopen("./books/books.txt","r")) == NULL){
+if ((checkfile = fopen("bcount.txt","r")) == NULL){
        printf("Error! opening file");
        // Program exits if the file pointer returns NULL.
        exit(1);
@@ -38,35 +26,12 @@ if ((checkfile = fopen("./books/books.txt","r")) == NULL){
 
 fscanf(checkfile,"%s",text);
 
-while (fgets(text, 100, checkfile) != NULL){
-    while (text[i] != ' '){
-	
-	if(text[i]>max){
-	max = text[i];
-        }
-        i++;
-    }
-
-    i=0;
-}
-
-max = max +1;
 
 
-fopen("./books/books.txt","w");
-
-if(createfile == NULL)
-   {
-      printf("Error!");   
-      exit(1);             
-   }
-
-snprintf(buf, 12, bname, max); // puts string into buffer
-
-fprintf(createfile,"%s",buf);
 
 
-fclose(createfile);
+
+
 
 
 
